1f9d28b714a9b2d45aab7c37e0f8946a2aca8b59
nbg-cordeiro <251023184@aluno.unb.br>
2026-01-28 11:20:40 -0300

    Adiciona novas funções auxiliares no header

--- headers/auxiliares.h
+++ headers/auxiliares.h
@@ -1,6 +1,9 @@
 #ifndef AUXILIARES_H
 #define AUXILIARES_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Node{
     void* conteudo;
     struct Node* proximo;
@@ -9,7 +12,9 @@ typedef struct Node{
 
 void limpaBuffer(void);
 void limpaConsole(void);
+char* lerString(void);
 void freeLista(Node** lista);
-void salvarLista(Node** lista, char* nomeArquivo);
+void salvarLista(Node*);
+Node* lerLista(char);
 
 #endif
